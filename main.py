a5bd1dede367f3e56a0c9302214fb58472e840c7
sudarshansvvs <sudarshansvvs@gmail.com>
2024-12-21 18:00:01 +0530

    completed video generation file with auido

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from Prompt_genrator import prompt_gen
 from story_generation import gemini_story_gen_format
 from playHT_TTS import playHT_AI_voice
 from image_generater import image_generater
+from video_generation import video_gen
 app = FastAPI()
 app.include_router(prompt_gen)
 app.include_router(gemini_story_gen_format)
 app.include_router(playHT_AI_voice)
 app.include_router(image_generater)
+app.include_router(video_gen)
 
--- video_generation.py
+++ video_generation.py
@@ -1,27 +1,92 @@
-from moviepy import AudioClip, CompositeAudioClip, CompositeVideoClip, ImageClip
-from moviepy.editor import *
+from moviepy import ImageClip, concatenate_videoclips, AudioFileClip, CompositeVideoClip, CompositeAudioClip
+import os
+import wave
+import random
 from pydub import AudioSegment
+from fastapi.params import Body
+from fastapi import APIRouter
 
-# Load images
-image1 = ImageClip("image1.jpg").set_duration(10)
-image2 = ImageClip("image2.jpg").set_duration(10)
 
-# Load audio files
-audio1 = AudioSegment.from_file("audio1.mp3")
-audio2 = AudioSegment.from_file("audio2.mp3")
-audio3 = AudioSegment.from_file("audio3.mp3")
+#list_loaded_audio = []
+list_loaded_image = []
+video_gen = APIRouter(
+    prefix=("/Post_video_gen"),
+    tags=["Post_video_gen"]
+)
+def export_mp3(data_audio):
+    data_audio.export("combined_audio.mp3", format="mp3") 
 
-# Create audio clips from audio segments
-audio_clip1 = AudioClip(audio1)
-audio_clip2 = AudioClip(audio2)
-audio_clip3 = AudioClip(audio3)
+def open_audio(audio_file):
+    try:
+        wave_file =  wave.open(audio_file, "rb")
+        final_audio_length = AudioSegment.from_file(audio_file).duration_seconds
+        print(f"audio_length of the the file {audio_file} is {final_audio_length}")
+        return AudioSegment.from_file(audio_file), final_audio_length
+    except  wave.Error:
+        get_audio(audio_file, final_audio_length)
 
-# Combine audio clips
-combined_audio = CompositeAudioClip([audio_clip1, audio_clip2, audio_clip3])
+def get_dire(data_path):
+    content_list =[]
+    for items in os.listdir(data_path):
+        item_path = os.path.join(data_path, items)
+        content_list.append(item_path)
+    return content_list
+def get_audio(data_path, final_audio_length):
+    list_loaded_audio = AudioSegment.empty()
+    print("data_path : Audio ", data_path)
+    content_audio=get_dire(data_path)
+    print("len(content_audio): ", len(content_audio))
+    for audio_file in content_audio:
+        loaded_audio, audio_length= open_audio(audio_file)
+        #list_loaded_audio.append(loaded_audio)
+        list_loaded_audio+=loaded_audio
+        final_audio_length+=audio_length
+    list_loaded_audio.export("tmp/combined_audio.wav", format="wav")
+    list_loaded = AudioFileClip("tmp/combined_audio.wav")
+    return list_loaded, final_audio_length
 
-# Combine video and audio clips
-final_clip = CompositeVideoClip([image1, image2])
-final_clip.audio = combined_audio
+def open_image(image_file, by_parts, final_audio_length):
+    try: 
+        _, ext = os.path.splitext(image_file)
+        if ext.lower() == '.jpg' or ext.lower() == '.jpeg':
+            loaded_image  = ImageClip(image_file).with_duration(by_parts)
+            return loaded_image
+    except Exception as e:
+        get_pics(image_file,final_audio_length)
 
-# Export the final video
-final_clip.write_videofile("output_video.mp4", fps=24)
+
+    
+
+def get_pics(data_path, final_audio_length):
+    print("data_path : ", data_path)
+    content_image=get_dire(data_path)
+    print('len(content_image) : ', len(content_image))
+    by_parts = int(final_audio_length/len(content_image))
+    print("by_parts : ", by_parts)
+    for image_file in content_image:
+        loaded_image= open_image(image_file, by_parts, final_audio_length)
+        list_loaded_image.append(loaded_image)
+    print(list_loaded_image)
+    list_image = [i for i in list_loaded_image if i!=None]
+    print(list_image)
+    return concatenate_videoclips(list_image)
+
+
+def make_video(Audio, pics):
+    print("Audio : ", Audio)
+    pics.audio = Audio
+    op= "video/op_video.mp4"
+    pics.write_videofile(op, fps=24)
+    return op
+
+
+@video_gen.post("/Post_video_gen")
+def Post_video_gen(data :dict = Body(...)):
+    Audio, final_audio_length  = get_audio(data["audio"],0)
+    print("final_audio_length  : ", final_audio_length) 
+    pics = get_pics(data["pics"], final_audio_length)
+    final_video = make_video(Audio, pics)
+    return final_video
+
+# b = {"audio":"audio/2024-12-17", "pics":"images/2024-12-17"}
+# Post_video_gen(b)
